cccce1e289c90898e24d2d4581b7d10a4916b43c
handhand <sohnjunior@naver.com>
2020-11-15 00:54:17 +0900

    [FE][chore] jest 설정파일 등록 #1

---
+++ client/src/__tests__/sample.spec.ts
@@ -0,0 +1,24 @@
+import hello from '@/components/demo';
+
+describe('Basic Types', () => {
+  test('Enum#number', () => {
+    enum Color {
+      Red,
+      Green,
+      Blue,
+    }
+    hello();
+    const c: Color = Color.Green;
+    expect(c).toBe(1);
+  });
+
+  test('Enum#name', () => {
+    enum Color {
+      Red = 1,
+      Green,
+      Blue,
+    }
+    const colorName: string = Color[2];
+    expect(colorName).toBe('Green');
+  });
+});
