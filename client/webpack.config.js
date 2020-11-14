f6bb4d191bf17bed485088d1e40d5bd6773f056f
handhand <sohnjunior@naver.com>
2020-11-14 16:56:35 +0900

    [FE][feat] webpack 설정 객체에서 함수로 변경
    + 옵션 값 활용을 위해 함수로 변경

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,11 +2,15 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: path.resolve(__dirname, "./src/index.ts"),
-  devtool: "inline-source-map",
-  module: {
+module.exports = (env, options) => {
+  const config = {};
+  const isDevelopment = options.mode === "development";
+
+  config.entry = path.resolve(__dirname, "./src/index.ts");
+  config.devtool = "inline-source-map";
+  config.module = {
     rules: [
       {
         test: /\.tsx?$/,
@@ -18,26 +22,44 @@ module.exports = {
         },
         exclude: /node_modules/,
       },
+      {
+        test: /\.s(a|c)ss$/,
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
     ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  output: {
+  };
+  config.resolve = {
+    extensions: [".ts", ".js", ".scss"],
+  };
+  config.output = {
     filename: "bundle.[hash].js",
     path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
+  };
+  config.plugins = [
     new ForkTsCheckerWebpackPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "./src/index.html"),
     }),
-  ],
-  devServer: {
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+    }),
+  ];
+  config.devServer = {
     host: "127.0.0.1",
     port: 8000,
     hot: true,
     contentBase: path.resolve(__dirname, "dist"),
-  },
+  };
+
+  return config;
 };
