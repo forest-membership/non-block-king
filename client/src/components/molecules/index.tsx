2ef7d3dc8a807ac5b2e33dbc828437151ad6b4a5
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-01-12 13:48:56 +0900

    [FE] 아토믹 패턴을 위한 디렉토리 추가 #7

---
+++ client/src/components/molecules/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Example = (): JSX.Element => {
+  return (
+    <div className="Example">
+      <span>HELLO WORLD!</span>
+    </div>
+  );
+};
+
+export default Example;
