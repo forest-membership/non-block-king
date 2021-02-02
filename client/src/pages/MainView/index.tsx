73f4f53a9a4f9457035b4478127feb3a997675ac
handhand <sohnjunior@naver.com>
2021-02-02 15:23:32 +0900

    [FE] feat: main view에 로그인 비동기 요청 추가 #19
    + 테스트용 목업 함수 requestAsync 사용 (추후 로그인 api 로 대체 필요)

--- client/src/pages/MainView/index.tsx
+++ client/src/pages/MainView/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import MainViewTemplate from './template';
 import NicknameForm from '../../components/molecules/NicknameForm';
+import useAsync from '../../hooks/useAsync';
+
+// TODO: 로그인 api로 대체 필요
+function requestAsync() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('success');
+    }, 3000);
+  });
+}
 
 function MainView(): JSX.Element {
-  return <MainViewTemplate nicknameForm={<NicknameForm />} />;
+  const [response, requestSignIn] = useAsync(requestAsync);
+  const { isLoading, data, error } = response;
+
+  const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await requestSignIn();
+  };
+
+  console.log(data); // FIXME: 디버깅용 로그
+
+  if (isLoading) return <div>로딩중</div>;
+  if (error) return <div>에러발생</div>;
+
+  return (
+    <MainViewTemplate nicknameForm={<NicknameForm onSubmit={handleSignIn} />} />
+  );
 }
 
 export default MainView;
