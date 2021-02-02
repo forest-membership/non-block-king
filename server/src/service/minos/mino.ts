f012f9bdbdb0ab38a2aa16ed0dc559c7c13b846e
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-02 13:12:18 +0900

    [BE] chore : mino에 사용되는 Block 클래스를 별도 파일 분리 #22

--- server/src/service/minos/mino.ts
+++ server/src/service/minos/mino.ts
@@ -1,24 +1,7 @@
-import Position from "@/types/position";
 import STATUS from "@/service/status";
+import Block from "@/service/minos/block";
 
 const TETRA = 4;
-
-class Block {
-    color?: string;
-    xPos: number;
-    yPos: number;
-
-    constructor(color?: string, xPos?: number, yPos?: number) {
-        this.color = color;
-        this.xPos = xPos || 0;
-        this.yPos = yPos || 0;
-    }
-
-    setPosition({ xPos, yPos }: Position) {
-        this.xPos = xPos;
-        this.yPos = yPos;
-    }
-}
 
 class Mino {
     protected blockArea: number[][];
