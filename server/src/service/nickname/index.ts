4d60184f5f23ca3f4e7b06a02cba780c7c425181
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-06 14:47:13 +0900

    [BE] feat : 닉네임 생성 함수 #27
    - 별도의 체크 로직 없이 생성해서 내보내는 기능
        - 서버 측에서 생성해서 제공하고,
        - 클라이언트에서 사용 확정 시에 다시 서버에게 알려주게 해야 한다.
        - 클라이언트는 제공된 것 외에도 닉네임을 쓸 수 있다.

---
+++ server/src/service/nickname/index.ts
@@ -0,0 +1,12 @@
+import adjective from "@/service/nickname/adjective";
+import noun from "@/service/nickname/noun";
+
+const adj = adjective.filter((word, index) => adjective.indexOf(word) === index);
+const name = noun.filter((word, index) => noun.indexOf(word) === index);
+
+const selectAdj = () => adj[Math.floor(Math.random() * adj.length)];
+const selectName = () => name[Math.floor(Math.random() * name.length)];
+
+const generateNickname = () => selectAdj() + " " + selectName();
+
+export default generateNickname;
