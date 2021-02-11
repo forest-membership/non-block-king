d05c01c3d608e8cd7aebea6839414400e809f72d
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-11 15:39:18 +0900

    [BE] feat : socket connection API #2

--- server/src/sockets/index.ts
+++ server/src/sockets/index.ts
@@ -1,24 +1,14 @@
-import io from "socket.io";
+import io from 'socket.io';
+import keyPressEvent from '@/sockets/gameController';
 
 const initSocket = (server: any) => {
-    const socketIO: io.Server = new io.Server(server);
-    socketIO.on("connect", (socket) => {
-        console.log("클라이언트와 연결되었습니다.");
-        socket.on("pressUpKey", () => {
-            console.log("업을 눌렀습니다.");
-        });
-        socket.on("pressDownKey", () => {
-            console.log("다운을 눌렀습니다.");
-        });
-        socket.on("pressLeftKey", () => {
-            console.log("왼쪽을 눌렀습니다.");
-        });
-        socket.on("pressRightKey", () => {
-            console.log("오른쪽을 눌렀습니다.");
-        });
-    });
+  const socketIO: io.Server = new io.Server(server);
+  socketIO.on('connect', (socket) => {
+    console.log('클라이언트와 연결되었습니다.');
+    keyPressEvent(socket);
+  });
 
-    return socketIO;
+  return socketIO;
 };
 
 export default initSocket;
