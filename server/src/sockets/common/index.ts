560a721303c473cc1f95f8efb3ebdb2216eaac6e
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-02-18 15:37:19 +0900

    [BE] feat : 사용자 맵 데이터 제공 및 입퇴장 유무 체크 #2

--- server/src/sockets/common/index.ts
+++ server/src/sockets/common/index.ts
@@ -3,3 +3,9 @@ export const sendMessageToUser = (
   userName: string,
   message: string
 ) => serverSocket.to(userName).emit('message', message);
+
+export const sendGameMapToUser = (
+  serverSocket: any,
+  userName: string,
+  map: number[][]
+) => serverSocket.to(userName).emit('gameMap', map);
--- server/src/sockets/roomManager/index.ts
+++ server/src/sockets/roomManager/index.ts
@@ -1,22 +1,34 @@
 import { Socket } from 'socket.io';
-import { sendMessageToUser } from '@/sockets/common';
+import { sendMessageToUser, sendGameMapToUser } from '@/sockets/common';
 import { addKeyPressEvent, removeKeyPressEvent } from '../gameController';
+import TetrisMap from '@/service/map';
 
 const roomManager = (serverSocket: any, socket: Socket, userNumber: number) => {
   const userName = `user:${userNumber}`;
+  const userGameMap = new TetrisMap();
+  let isEntered = false;
 
   socket.on('join', () => {
+    if (isEntered) {
+      return;
+    }
     socket.join('room1');
     sendMessageToUser(serverSocket, userName, '입장하였습니다.');
-
+    const mapState = userGameMap.offerUserMap;
+    sendGameMapToUser(serverSocket, userName, mapState);
     // TODO : 게임이 시작됨을 알리는 소켓 이벤트 위치로 이동시킬 것.
     addKeyPressEvent(serverSocket, socket, userNumber);
+    isEntered = true;
   });
 
   socket.on('quit', () => {
+    if (!isEntered) {
+      return;
+    }
     socket.leave('room1');
     sendMessageToUser(serverSocket, userName, '퇴장하였습니다.');
     removeKeyPressEvent(socket);
+    isEntered = false;
   });
 
   socket.on('ready', () => {
