8b47493bc7753865b62f97b6d1ec311cac98c671
handhand <sohnjunior@naver.com>
2021-03-11 11:26:00 +0900

    [BE] feat: cors 이슈 대응 #42

--- server/src/sockets/index.ts
+++ server/src/sockets/index.ts
@@ -2,8 +2,15 @@ import http from 'http';
 import io, { Socket } from 'socket.io';
 import * as ModeManager from '@/sockets/modeManager';
 
+const CLIENT_ORIGIN = 'http://127.0.0.1:8000';
+
 export default function initSocket(server: http.Server) {
-  const serverSocket: io.Server = new io.Server(server);
+  const serverSocket: io.Server = new io.Server(server, {
+    cors: {
+      origin: CLIENT_ORIGIN,
+      methods: ['GET', 'POST'],
+    },
+  });
 
   serverSocket.on('connection', (client: Socket) => {
     console.log(`${client.id} 님이 연결되었습니다.`);
