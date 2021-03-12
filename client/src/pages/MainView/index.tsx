497ef62d8676936be146e8e71c16fc13774ec6ab
handhand <sohnjunior@naver.com>
2021-03-13 00:35:47 +0900

    [FE] feat: main 뷰 소켓 연결 및 더미 데이터 리팩토링 #42
    + 더미 데이터 seeds 디렉토리로 이동

--- client/src/pages/MainView/index.tsx
+++ client/src/pages/MainView/index.tsx
@@ -1,11 +1,13 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import MainViewTemplate from './template';
 import NicknameForm from '../../components/molecules/NicknameForm';
 import ModeSelectBox from '../../components/molecules/ModeSelectBox';
 import RankingList from '../../components/organisms/RankingList';
 import useAsync from '../../hooks/useAsync';
+import useSocket from '../../hooks/useSocket';
 import AuthAPI from '../../apis/auth';
+import { rankers } from '../../seeds/ranking';
 
 // TODO: 로그인 api로 대체 필요
 function requestAsync() {
@@ -16,22 +18,26 @@ function requestAsync() {
   });
 }
 
-// TODO: 실제 랭킹 API와 연동 필요
-const dummy = [
-  { ordinal: '1st', username: 'apple', score: 5000 },
-  { ordinal: '2nd', username: 'banana', score: 4000 },
-  { ordinal: '3th', username: 'pineapple', score: 3000 },
-  { ordinal: '4th', username: 'grape', score: 2000 },
-  { ordinal: '5th', username: 'peach', score: 1000 },
-];
-
 function MainView(): JSX.Element {
   const history = useHistory();
   const [signinResponse, requestSignIn] = useAsync(requestAsync);
   const [nicknameResponse, requestNickname] = useAsync(AuthAPI.getNickname);
+  const { socket, connect } = useSocket();
 
   const { isLoading, data: token, error: signinError } = signinResponse;
   const { data: issuedNickname, error: nicknameError } = nicknameResponse;
+
+  useEffect(() => {
+    const isLoginSuccess = token !== null;
+
+    if (isLoginSuccess) {
+      connect(socket);
+      socket.on('connect', () => {
+        /** TODO: 토스트 팝업으로 띄우기 */
+        console.log('서버 접속 성공');
+      });
+    }
+  }, [token]);
 
   const handleSignIn = (e: React.FormEvent<HTMLFormElement>) => {
     /** TODO: issuedNickname 활용해서 로그인 API 요청 */
@@ -39,8 +45,13 @@ function MainView(): JSX.Element {
     requestSignIn();
   };
 
-  const handleModeSelect = useCallback((mode: string) => {
-    history.push(`/${mode}`);
+  const handleModeSelect = useCallback((mode: 'pvp' | 'pve' | 'pvf') => {
+    /** TODO: 토스트 팝업으로 띄우기 */
+    socket.emit(`join:${mode}`);
+    socket.on('join:success', (message: string) => {
+      console.log(message);
+      history.push(`/${mode}`);
+    });
   }, []);
 
   const handleGenerateNickname = useCallback(
@@ -65,8 +76,8 @@ function MainView(): JSX.Element {
         />
       }
       modeSelectBox={<ModeSelectBox onSelect={handleModeSelect} />}
-      rankingListLeft={<RankingList rankingItems={dummy} />}
-      rankingListRight={<RankingList rankingItems={dummy} />}
+      rankingListLeft={<RankingList rankingItems={rankers} />}
+      rankingListRight={<RankingList rankingItems={rankers} />}
     />
   );
 }
---
+++ client/src/seeds/ranking/index.ts
@@ -0,0 +1,7 @@
+export const rankers = [
+  { ordinal: '1st', username: 'apple', score: 5000 },
+  { ordinal: '2nd', username: 'banana', score: 4000 },
+  { ordinal: '3th', username: 'pineapple', score: 3000 },
+  { ordinal: '4th', username: 'grape', score: 2000 },
+  { ordinal: '5th', username: 'peach', score: 1000 },
+];
