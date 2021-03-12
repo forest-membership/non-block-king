74b405a6e06e8c06288919c33a4c849dd9d35550
handhand <sohnjunior@naver.com>
2021-03-13 00:01:35 +0900

    [BE] fix: 다음 미노 미리보기 반환 함수 버그 수정 #42
    + 고차함수 관련 버그 수정

--- server/src/service/minos/index.ts
+++ server/src/service/minos/index.ts
@@ -1,18 +1,23 @@
-import IMino from "@/service/minos/I.mino";
-import JMino from "@/service/minos/J.mino";
-import LMino from "@/service/minos/L.mino";
-import OMino from "@/service/minos/O.mino";
-import SMino from "@/service/minos/S.mino";
-import TMino from "@/service/minos/T.mino";
-import ZMino from "@/service/minos/Z.mino";
+import IMino from '@/service/minos/I.mino';
+import JMino from '@/service/minos/J.mino';
+import LMino from '@/service/minos/L.mino';
+import OMino from '@/service/minos/O.mino';
+import SMino from '@/service/minos/S.mino';
+import TMino from '@/service/minos/T.mino';
+import ZMino from '@/service/minos/Z.mino';
 
-const initMinos = () => [new IMino(), new JMino(), new LMino(), new OMino(), new SMino(), new TMino(), new ZMino()];
+function getNextMinos() {
+  const minos = [
+    new IMino(),
+    new JMino(),
+    new LMino(),
+    new OMino(),
+    new SMino(),
+    new TMino(),
+    new ZMino(),
+  ];
 
-const shuffleMinos = () => {
-    const shuffle = () => initMinos().sort(() => Math.random() - 0.5);
-    return shuffle;
-};
-
-const getNextMinos = shuffleMinos();
+  return minos.sort(() => Math.random() - 0.5);
+}
 
 export default getNextMinos;
