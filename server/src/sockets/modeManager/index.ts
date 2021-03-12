c1e4f0fb7fbbb24c1e155fb910f825dae4fa664a
handhand <sohnjunior@naver.com>
2021-03-13 00:10:41 +0900

    [BE] refactor: 이벤트 명 컨벤션 변경 #42

--- server/src/sockets/modeManager/index.ts
+++ server/src/sockets/modeManager/index.ts
@@ -4,21 +4,33 @@ import * as RoomManager from '../roomManager';
 
 function onJoinPvP(this: Socket) {
   RoomManager.allocateRoom(this, 'PvP');
-  MessageManager.sendMessageToUser(this, 'PvP에 입장하셨습니다. 😄');
+  MessageManager.sendMessageToClient(
+    this,
+    'join:success',
+    'PvP에 입장하셨습니다. 😄'
+  );
 }
 
 function onJoinPvF(this: Socket) {
   RoomManager.allocateRoom(this, 'PvF');
-  MessageManager.sendMessageToUser(this, 'PvF에 입장하셨습니다. 😄');
+  MessageManager.sendMessageToClient(
+    this,
+    'join:success',
+    'PvF에 입장하셨습니다. 😄'
+  );
 }
 
 function onJoinPvE(this: Socket) {
   RoomManager.allocateRoom(this, 'PvE');
-  MessageManager.sendMessageToUser(this, 'PvE에 입장하셨습니다. 😄');
+  MessageManager.sendMessageToClient(
+    this,
+    'join:success',
+    'PvE에 입장하셨습니다. 😄'
+  );
 }
 
 export function attachJoinEvents(client: Socket) {
-  client.on('joinPvP', onJoinPvP);
-  client.on('joinPvF', onJoinPvF);
-  client.on('joinPvE', onJoinPvE);
+  client.on('join:pvp', onJoinPvP);
+  client.on('join:pvf', onJoinPvF);
+  client.on('join:pve', onJoinPvE);
 }
--- server/src/sockets/roomManager/index.ts
+++ server/src/sockets/roomManager/index.ts
@@ -1,6 +1,6 @@
 import { Socket } from 'socket.io';
 import * as MessageManager from '@/sockets/messageManager';
-import * as EventManager from '@/sockets/eventManager';
+import * as KeyEventManager from '@/sockets/keyEventManager';
 import * as GameManager from '@/sockets/gameManager';
 import { generateRandomString } from '@/utils';
 
@@ -11,7 +11,7 @@ function generateRoomHashCode() {
 }
 
 function onReady(this: Socket) {
-  MessageManager.sendMessageToUser(this, '게임을 준비하였습니다.');
+  /** TODO: 현재 준비중인 유저 수 클라이언트에게 전송해주기 */
 }
 
 function onStart(this: Socket) {
@@ -19,34 +19,39 @@ function onStart(this: Socket) {
   const isAllPlayerOnReady = true;
   if (!isAllPlayerOnReady) return;
 
+  KeyEventManager.attachKeyPressEvents(this);
   // FIXME: 해당 방의 모든 참가자에게 게임 인스턴스 할당하는 것으로 변경
-  GameManager.initGameMap(this.id);
-  EventManager.attachKeyPressEvents(this);
-  MessageManager.sendMessageToUser(this, '게임 시작! 🔥');
+  GameManager.initGameMap(this);
+  GameManager.startGame(this);
 }
 
-function onLose(this: Socket) {
-  EventManager.detachKeyPressEvents(this);
-  MessageManager.sendMessageToUser(this, '패배하였습니다. 😫');
-}
+// FIXME: 패배 이벤트 클라이언트에서 받는것이 아니라 서버에서 처리 후 클라이언트에게 전송해야 함
+// 따라서 roomManager가 아니라 다른 곳으로 이동해야함
+// function onLose(this: Socket) {
+//   EventManager.detachKeyPressEvents(this);
+//   MessageManager.sendMessageToClient(this, 'room:lose', '패배하였습니다. 😫');
+// }
 
 function onQuit(this: Socket) {
   this.leave('room1'); // FIXME: room1 대신 실제 roomCode로 바꿔야함
-  MessageManager.sendMessageToUser(this, '퇴장하였습니다. 👋');
 }
 
 function attachRoomEvents(client: Socket) {
-  client.on('ready', onReady);
-  client.on('start', onStart);
-  client.on('lose', onLose);
-  client.on('quit', onQuit);
+  client.on('room:ready', onReady);
+  client.on('room:start', onStart);
+  client.on('room:quit', onQuit);
 }
 
 // TODO: RoomManager 가 유저가 입장 요청을 보냈을 때 적절한 방을 찾아서 매칭해주도록 한다.
+// TODO: 접속자를 제외한 해당 roomcode 의 모든 유저들에게 메시지를 emit 하도록 한다.
 export function allocateRoom(client: Socket, mode: string) {
   const roomCode = `${mode}:${generateRoomHashCode()}`;
 
   client.join(roomCode);
   attachRoomEvents(client);
-  MessageManager.sendMessageToUser(client, '입장하였습니다. 👋');
+  MessageManager.sendMessageToClient(
+    client,
+    'room:enter',
+    '입장하였습니다. 👋'
+  );
 }
