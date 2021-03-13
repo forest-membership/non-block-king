aa0f2b327b723397e7841eff5163fdb2acc18852
handhand <sohnjunior@naver.com>
2021-03-14 02:39:08 +0900

    [BE] refactor: 미노 set 생성 함수 파일명 변경 #42

---
+++ server/src/service/game/minos/helper.ts
@@ -0,0 +1,21 @@
+import IMino from '@/service/game/minos/IMino';
+import JMino from '@/service/game/minos/JMino';
+import LMino from '@/service/game/minos/LMino';
+import OMino from '@/service/game/minos/OMino';
+import SMino from '@/service/game/minos/SMino';
+import TMino from '@/service/game/minos/TMino';
+import ZMino from '@/service/game/minos/ZMino';
+
+export function getNextMinos() {
+  const minos = [
+    new IMino(),
+    new JMino(),
+    new LMino(),
+    new OMino(),
+    new SMino(),
+    new TMino(),
+    new ZMino(),
+  ];
+
+  return minos.sort(() => Math.random() - 0.5);
+}
